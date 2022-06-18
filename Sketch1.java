5047071addc14cd92f86ef56d95e384478a97177
Ellis Guo <99352765+ellisguo@users.noreply.github.com>
2022-06-18 03:51:16 +0000

    homework

--- Sketch1.java
+++ Sketch1.java
@@ -1,36 +1,94 @@
-import processing.core.PApplet;
+/*
 
-public class Sketch1 extends PApplet {
+import processing.core.PApplet;
+import processing.core.PImage;
+import java.util.ArrayList;
+
+public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+	float snakeX = 150;
+  float snakeY = 100;
+
+  boolean upPressed = false;
+  boolean downPressed = false;
+  boolean leftPressed = false;
+  boolean rightPressed = false;
+
+  
   public void settings() {
-	// put your size call here
+	
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
     background(210, 255, 173);
   }
-
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
+  
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    fill(0,255,0);
+    noStroke();
+    ellipse(snakeX, snakeY, 20, 20);
+
+    if (upPressed) {
+      snakeY-= 2;
+    }
+    if (downPressed) {
+      snakeY+=2;
+    }
+    if (leftPressed) {
+      snakeX-=2;
+    }
+    if (rightPressed) {
+      snakeX+=2;
+    }
+    
+    if (snakeX < 0 || snakeX > width) {
+      background(255);
+      fill(0);
+      textSize(50);
+      text("GAME END", 30, 55);
+    }
+  
+    if (snakeY < 0  || snakeY > height) {
+      background(255);
+      fill(0);
+      textSize(50);
+      text("GAME END", 30, 55);
+    }
+	
+  }
+
+  public void keyPressed() {
+    
+  if (keyCode == UP) {
+    upPressed = true;
+    downPressed = false;
+    leftPressed = false;
+    rightPressed = false;
+  }
+  else if (keyCode == DOWN) {
+    downPressed = true;
+    upPressed = false;
+    leftPressed = false;
+    rightPressed = false;
+  }
+  else if (keyCode == LEFT) {
+    leftPressed = true;
+    downPressed = false;
+    upPressed = false;
+    rightPressed = false;
+  }
+  else if (keyCode == RIGHT) {
+    rightPressed = true;
+    downPressed = false;
+    leftPressed = false;
+    upPressed = false;
+  }
+    
   }
   
-  // define other methods down here.
+  
 }
+
+*/
